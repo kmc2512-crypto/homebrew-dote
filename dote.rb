5bbc59a241f3b4073087ed33406aa1e276e92caa
kmc2512-crypto <kmc2512@kamiyama.ac.jp>
2025-10-28 13:59:22 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/kmc2512-crypto/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2512-crypto/homebrew-dote/COMMIT_SHA/dote.c"
-  sha256 "SHA256_OF_RAW_AT_COMMIT"
+  sha256 "a3c0618830ce0976466fa15ec582c3800a196dc3bbf7e7ad6b14a8f745c89141"
   version "1.0.0"
   license "MIT"
 
