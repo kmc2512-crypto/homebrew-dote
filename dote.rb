fa51a5bfdb476895f42fca7c60c3a2fe87953eff
kmc2512-crypto <kmc2512@kamiyama.ac.jp>
2025-10-28 13:53:41 +0900

    version “1.0.0”

--- dote.rb
+++ dote.rb
@@ -1,8 +1,9 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/kmc2400/homebrew-dote"
-  url "https://raw.githubusercontent.com/kmc2400/homebrew-dote/main/dote.c"
-  sha256 "PUT_SHA256_OF_DOTE.C_HERE"
+  homepage "https://github.com/kmc2512-crypto/homebrew-dote"
+  url "https://raw.githubusercontent.com/kmc2512-crypto/homebrew-dote/main/dote.c"
+  sha256 "a3c0618830ce0976466fa15ec582c3800a196dc3bbf7e7ad6b14a8f745c89141"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
